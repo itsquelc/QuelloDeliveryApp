d83a7bd543b888691e86554bb9400dcfaf1d8a68
itsquelc <raquelnunescirino@gmail.com>
2025-09-22 07:55:36 -0300

    sdf

--- QuelloDelivery/components/BottomSheet.tsx
+++ QuelloDelivery/components/BottomSheet.tsx
@@ -1,46 +1,56 @@
-import { StyleSheet, View, Text, TouchableOpacity } from 'react-native'
-import React, { forwardRef, useCallback, useMemo } from 'react'
-import { BottomSheetModal, BottomSheetView, BottomSheetBackdrop, useBottomSheetModal } from '@gorhom/bottom-sheet'
-import Colors from '@/constants/Colors';
-import Ionicons from '@expo/vector-icons/Ionicons';
-import { Link } from 'expo-router';
+import { StyleSheet, View, TouchableOpacity, Text } from "react-native"
+import React, { forwardRef, useCallback,  useMemo } from "react"
+import {  BottomSheetBackdrop, BottomSheetModal, BottomSheetView, useBottomSheetModal } from "@gorhom/bottom-sheet"
+import Colors from "@/constants/Colors";
+import Ionicons from "@expo/vector-icons/Ionicons";
+import { Link } from "expo-router";
 
 export type Ref = BottomSheetModal;
 
 const BottomSheet = forwardRef<Ref>((props, ref) => {
     const snapPoints = useMemo(() => ['50%'], []);
-    const renderBackdrop = useCallback((props: any) =>
-        <BottomSheetBackdrop appearsOnIndex={0} disappearsOnIndex={-1} {...props}/>, [])
-    const {dismiss} = useBottomSheetModal();
-    return(
-        <BottomSheetModal ref={ref} 
-        snapPoints={snapPoints}
+    const renderBackdrop = useCallback((props: any) => 
+        <BottomSheetBackdrop appearsOnIndex={0} disappearsOnIndex={-1} {...props} />      , [])
+    const  { dismiss } = useBottomSheetModal();
+    return (
+        <BottomSheetModal ref={ref} snapPoints={snapPoints}
         backdropComponent={renderBackdrop}
         overDragResistanceFactor={0}
         handleIndicatorStyle={{display: 'none'}}
-        backgroundStyle={{backgroundColor : Colors.lightGrey, borderRadius: 0}}
-        >
+        backgroundStyle={{backgroundColor: Colors.lightGrey, borderRadius: 0}}>
+            
+            
             <BottomSheetView style={styles.contentContainer}>
-               <View style={styles.toggle}>
-                <TouchableOpacity style={{styles.toggleButton, styles.toogleActive}}>
-                    <Text style={activeText}>entrega</Text>
-                </TouchableOpacity>
-                <TouchableOpacity style={{styles.toggleButton, styles.toogleInactive}}>
-                    <Text style={inactiveText}>retirada</Text>
-                </TouchableOpacity>
-               </View>
+                <View style={styles.toggle}>
+                    <TouchableOpacity style={[styles.toggleButton, styles.toggleActive]}>
+                        <Text style={styles.activeText}>Entrega</Text>
+                    </TouchableOpacity>
+                    <TouchableOpacity style={[styles.toggleButton, styles.toggleInactive]}>
+                        <Text style={styles.inactiveText}>Entrega</Text>
+                    </TouchableOpacity>
 
-                <Text style={styles.subheader}>Minha Localização</Text>
-               <Link href={'/'} asChild>
-               <TouchableOpacity style={styles.item}>
-                    <Ionicons name='location-outline' size={20} color={Colors.medium} />
-                    <Text style={{ flex: 1 }}>Localização atual</Text>
-                    <Ionicons name="chevron-forward" size={20} color={Colors.primary}/>
-               </TouchableOpacity>
-               </Link>
-               
-                <TouchableOpacity onPress={ () => dismiss() }>
-                    <Text>Fechar</Text>
+                </View>
+
+                <Text style={styles.subheader}> Minha Localização</Text>
+                <Link href={'/'} asChild>
+                <TouchableOpacity style={styles.item}>
+                    <Ionicons name="location-outline" size={28} color={Colors.medium} />
+                    <Text style={{ flex: 1}}>Localização atual</Text>
+                    <Ionicons name="chevron-forward" size={20} color={Colors.primary} />
+                </TouchableOpacity>
+                </Link>
+
+                <Text style={styles.subheader}>Tempo para entrega</Text>
+                <Link href={'/'} asChild>
+                <TouchableOpacity style={styles.item}>
+                    <Ionicons name="location-outline" size={28} color={Colors.medium} />
+                    <Text style={{ flex: 1}}>Agora</Text>
+                    <Ionicons name="chevron-forward" size={20} color={Colors.primary} />
+                </TouchableOpacity>
+                </Link>
+
+                <TouchableOpacity style={styles.button} onPress={ () => dismiss() }>
+                    <Text style={styles.buttonText}>Confirmar</Text>
                 </TouchableOpacity>
             </BottomSheetView>
         </BottomSheetModal>
@@ -51,7 +61,56 @@ export default BottomSheet
 
 const styles = StyleSheet.create({
     contentContainer: {
-        alignItems: 'center',
         height: '100%',
+        paddingHorizontal: 16,
     },
+    toggle: {
+        flexDirection: 'row',
+        justifyContent: 'center',
+        gap: 20,
+        marginBottom: 32,
+    },
+    toggleButton: {
+        paddingVertical: 4,
+        paddingHorizontal: 30,
+        borderRadius: 32,
+    },
+    toggleActive: {
+        backgroundColor: Colors.primary
+    },
+    activeText: {
+        color: '#fff',
+        fontWeight: '700',
+
+    },
+    toggleInactive: {},
+    inactiveText: {
+        color: Colors.primary
+    },
+    subheader: {
+        fontSize: 16,
+        fontWeight: '600',
+        marginBottom: 10,
+    },
+    item: {
+        flexDirection: 'row',
+        gap: 8,
+        alignItems: 'center',
+        padding: 16,
+        borderColor: Colors.grey,
+        borderWidth: 1,
+        marginBottom: 16,
+    },
+    button: {
+        backgroundColor: Colors.primary,
+        padding: 12,
+        borderRadius: 8,
+        alignItems: 'center',
+        marginTop: 10,
+    },
+    buttonText: {
+        color:'#fff',
+        fontWeight: 'bold',
+    }
+
 })
